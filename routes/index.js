30971486e764c43a2dc0503973320eccd442f2c8
Woh Shon Phoon <wohshon@google.com>
2021-05-18 13:32:10 +0800

    show ip

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var ip = require("ip");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
-     var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null;
-   console.log(ip);
-  res.status(200).send("OK: "+ip);
+     var clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null;
+   console.log(clientIp);
+  res.status(200).send("OK: "+ip.address()+" "+clientIp);
 });
 
 module.exports = router;
